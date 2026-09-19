344c899f4c3e55e4a54249417fda95105cc96275
Gulshan Kumar <gulsanshakya123@gmail.com>
2026-09-19 20:18:20 +0530

    Add script to display name and age message

---
+++ mini-scripts/Basic/7_name_age_message.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Enter your name: " name
+read -p "Enter your age: " age
+
+echo "-------------------------"
+echo "Name : $name"
+echo "Age  : $age"
+echo "-------------------------"
+echo "Hello $name, you are $age years old."
